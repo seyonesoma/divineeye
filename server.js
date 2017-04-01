efc149b8d496433fde2877a3973083c21c557504
seyonesoma <seyone@divineeye.ca>
2017-04-01 14:14:21 -0400

    Update server.js

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-   res.send('Hello World');
-   res.send('testing 12 3 ')
+   res.send('Hello World testing 123');
+
 })
 
 var server = app.listen(8081, function () {
